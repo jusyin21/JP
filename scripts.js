da54045d56852fb4fd285d399dad9d677817a8ed
jusyin21 <119901307+jusyin21@users.noreply.github.com>
2025-03-27 04:17:57 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Fade in elements with the fade-in class
-    var fadeIns = document.querySelectorAll('.fade-in');
-    fadeIns.forEach(function(element) {
-      element.style.animationPlayState = 'running';
-    });
-  });
-  
+const toggle = document.createElement('button');
+toggle.classList.add('absolute', 'top-5', 'right-5', 'bg-teal-400', 'text-white', 'p-2', 'rounded-full');
+toggle.textContent = 'Toggle Mode';
+document.body.appendChild(toggle);
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
